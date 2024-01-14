e0afc5ed0ea447c9a7d8f9955b93416606513630
erandaka.bandara <erandakaanupama@gmail.com>
2024-01-14 07:32:00 +0530

    Initial commit

---
+++ src/main/java/com/eab/myspaceproducts/MyspaceProductsApplication.java
@@ -0,0 +1,13 @@
+package com.eab.myspaceproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspaceProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspaceProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eab/myspaceproducts/MyspaceProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eab.myspaceproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspaceProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
