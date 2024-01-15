f0392d9dbf5754a0c8bf9f9c333165ba739836b5
erandaka.bandara <erandakaanupama@gmail.com>
2024-01-15 19:01:49 +0530

    [MYS-1] fix: remove public access modifier for interface

--- src/main/java/com/eab/myspaceproducts/mapper/BaseMapper.java
+++ src/main/java/com/eab/myspaceproducts/mapper/BaseMapper.java
@@ -7,7 +7,7 @@ package com.eab.myspaceproducts.mapper;
  */
 public interface BaseMapper<E, D> {
 
-    public E mapToEntity(D dto);
+    E mapToEntity(D dto);
 
-    public D mapFromEntity(E entity);
+    D mapFromEntity(E entity);
 }
