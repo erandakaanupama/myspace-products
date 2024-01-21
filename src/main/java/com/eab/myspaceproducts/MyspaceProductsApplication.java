af11430cf2cd26e2ffda0c581ee61c026a728a3f
erandaka.bandara <erandakaanupama@gmail.com>
2024-01-21 08:34:24 +0530

    feat: introduce jpa audit support

--- src/main/java/com/eab/myspaceproducts/MyspaceProductsApplication.java
+++ src/main/java/com/eab/myspaceproducts/MyspaceProductsApplication.java
@@ -2,8 +2,10 @@ package com.eab.myspaceproducts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class MyspaceProductsApplication {
 
 	public static void main(String[] args) {
