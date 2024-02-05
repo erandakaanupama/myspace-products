fc225cf9b9b248e2e48e469895a0119cd6413518
Erandaka Anupama Bandara <erandakaanupama@gmail.com>
2024-02-05 07:45:22 +0530

    feat: refresh config at runtime (#8)

--- src/main/java/com/eab/myspaceproducts/dto/ProductContactInformationDto.java
+++ src/main/java/com/eab/myspaceproducts/dto/ProductContactInformationDto.java
@@ -1,11 +1,19 @@
 package com.eab.myspaceproducts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "products")
-public record ProductContactInformationDto(String message, Map<String, String> contactDetails,
-                                           List<String> onCallSupport) {
+@Getter
+@Setter
+public class ProductContactInformationDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
+
+
